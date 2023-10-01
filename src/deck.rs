5116a406366452d46bd7d45a270ffe2beb5adc48
Nick Watkins <imnickwatkins@gmail.com>
2023-10-01 00:52:03 -0400

    taking a crack at creating a shuffled deck

--- src/deck.rs
+++ src/deck.rs
@@ -1,5 +1,10 @@
+use rand::thread_rng;
+use rand::seq::SliceRandom;
+use itertools::Itertools;
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
 
-
+#[derive(Debug, EnumIter)]
 enum Suit{
     Heart,
     Diamond,
@@ -19,7 +24,7 @@ struct PlayerHand {
 impl Card{
     /// This function creates a new card with the value and suit specified, 
     /// and returns a BoundError if the value is not a valid integer.
-    pub const fn new(value: i8, suit:Suit) -> Resukt<Self, BoundError>{
+    pub const fn new(value: i8, suit:Suit) -> Result<Self, BoundError>{
         if value >= 1 && value <= 13 {
             Ok(Self(value, suit))
         } else {
@@ -29,16 +34,27 @@ impl Card{
 }
 
 struct Community {
-    flop: list(Card, Card, Card),
+    flop: std::Vec<Card>,
     turn: Card,
     river: Card,
 }
 
 struct Discard {
-    pile: (Card)
+    pile: std::Vec<Card>
 }
 
 struct Deck{
-
+    shoe: std::Vec<Card>
 }
 
+impl Deck{
+    const fn new() -> Self{
+        // create a standard 52 card deck   
+        let mut shoe: Vec<Card> = iproduct!(1..14, Suit::iter())
+            .map(|(s, v)| Card{value:v, suit:s})
+            .collect();
+    }
+    const fn shuffle() -> Self{
+        shoe.shuffle(&mut thread_rng());
+    }
+}
